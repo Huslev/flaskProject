222544e5f6d404611425f85e3301442b10830255
glebb <Huslev1244>
2022-03-30 21:01:30 +0300

    just start

---
+++ app.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/home')
+def index():  # put application's code here
+    return render_template("index.html")
+
+@app.route('/')
+@app.route('/mainpage')
+def mainpage():  # put application's code here
+    return render_template("home.html")
+
+@app.route('/')
+@app.route('/action')
+def actionpage():  # put application's code here
+    return render_template("action.html")
+
+@app.route('/')
+@app.route('/bees_stat')
+def beespage():  # put application's code here
+    return render_template("bees_stat.html")
+
+@app.route('/')
+@app.route('/streampage')
+def beespage():  # put application's code here
+    return render_template("stream.html")
+
+@app.route('/')
+@app.route('/product_status')
+def productpage():  # put application's code here
+    return render_template("product_status.html")
+
+@app.route('/')
+@app.route('/product_status')
+def productpage():  # put application's code here
+    return render_template("product_status.html")
+
+
+@app.route('/')
+@app.route('/documents')
+def documentpage():  # put application's code here
+    return render_template("documents.html")
+
+
+@app.route('/')
+@app.route('/documents')
+def documentpage():  # put application's code here
+    return render_template("documents.html")
+
+
+
+@app.route('/')
+@app.route('/documents')
+def abautpage():  # put application's code here
+    return render_template("documents.html")
+
+
+if __name__ == '__main__':
+    app.run()
